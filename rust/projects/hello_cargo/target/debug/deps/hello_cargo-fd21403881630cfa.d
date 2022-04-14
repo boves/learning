f416339bc2ed6536073cbbfa5040953add2804f3
boves <daiv.boveri@gmail.com>
2022-04-14 09:12:43 -0500

    Created first project using cargo in rust

---
+++ rust/projects/hello_cargo/target/debug/deps/hello_cargo-fd21403881630cfa.d
@@ -0,0 +1,5 @@
+/Users/daiv/coding/learning/rust/projects/hello_cargo/target/debug/deps/hello_cargo-fd21403881630cfa: src/main.rs
+
+/Users/daiv/coding/learning/rust/projects/hello_cargo/target/debug/deps/hello_cargo-fd21403881630cfa.d: src/main.rs
+
+src/main.rs:
---
+++ rust/projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/daiv/coding/learning/rust/projects/hello_cargo/target/debug/hello_cargo: /Users/daiv/coding/learning/rust/projects/hello_cargo/src/main.rs
