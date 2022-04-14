78da2523e308433be7f5c015bfc0553990f8f2e5
boves <daiv.boveri@gmail.com>
2022-04-14 09:29:20 -0500

    Added missing files from hell_cargo in rust

---
+++ rust/projects/hello_cargo/target/debug/deps/hello_cargo-b455c883bae505bd.d
@@ -0,0 +1,5 @@
+/Users/daiv/coding/learning/rust/projects/hello_cargo/target/debug/deps/hello_cargo-b455c883bae505bd.rmeta: src/main.rs
+
+/Users/daiv/coding/learning/rust/projects/hello_cargo/target/debug/deps/hello_cargo-b455c883bae505bd.d: src/main.rs
+
+src/main.rs:
---
+++ rust/projects/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/daiv/coding/learning/rust/projects/hello_cargo/target/release/hello_cargo: /Users/daiv/coding/learning/rust/projects/hello_cargo/src/main.rs
