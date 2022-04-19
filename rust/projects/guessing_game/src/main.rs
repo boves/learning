c1ad677cb5d1b91d8493d7d7cb751bfc84027e1d
boves <daiv.boveri@gmail.com>
2022-04-18 19:33:08 -0500

    [In progress] Implementing Looping for rust guessing game

--- rust/projects/guessing_game/src/main.rs
+++ rust/projects/guessing_game/src/main.rs
@@ -11,16 +11,21 @@ fn main(){
 
     let mut guess = String::new(); // mutable
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    loop{
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
-    match guess.cmp(&secret_number){
-        Ordering::Less    => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal   => println!("You win!"),
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number){
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                                  println!("You win!");
+                break;
+            }
+        }
     }
 }
