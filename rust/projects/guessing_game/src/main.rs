858f056b520ffed8c934ef11fd872172d8dd164a
boves <daiv.boveri@gmail.com>
2022-04-22 20:21:34 -0500

    Fixed looping error in Guessing_Game

--- rust/projects/guessing_game/src/main.rs
+++ rust/projects/guessing_game/src/main.rs
@@ -1,17 +1,21 @@
 use std::io;
+use std::env;
 use rand::Rng;
 use std::cmp::Ordering;
 
 fn main(){
+    // env::set_var("RUST_BACKTRACE","1");
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101); //immutable
 
     println!("The secret number is: {}", secret_number);
     println!("Please input your guess.");
 
-    let mut guess = String::new(); // mutable
+    // let mut guess = String::new(); // mutable
 
     loop{
+        
+        let mut guess = String::new(); // mutable
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
