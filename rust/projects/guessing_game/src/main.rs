025743318280fb6cf303076005b363286d2cb2fd
boves <daiv.boveri@gmail.com>
2022-04-23 15:41:28 -0500

    Minor update: final change to guessing_game

--- rust/projects/guessing_game/src/main.rs
+++ rust/projects/guessing_game/src/main.rs
@@ -8,7 +8,7 @@ fn main(){
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101); //immutable
 
-    println!("The secret number is: {}", secret_number);
+    // println!("The secret number is: {}", secret_number);
     println!("Please input your guess.");
 
     // let mut guess = String::new(); // mutable
