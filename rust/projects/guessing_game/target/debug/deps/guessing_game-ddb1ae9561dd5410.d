67a093df356d6debeda8b77d29cecac239e85f86
boves <daiv.boveri@gmail.com>
2022-04-22 20:36:04 -0500

    Start ignoring /target/* files from all rust projects

--- rust/projects/guessing_game/target/debug/deps/guessing_game-ddb1ae9561dd5410.d
+++
@@ -1,5 +0,0 @@
-/Users/daiv/coding/learning/rust/projects/guessing_game/target/debug/deps/guessing_game-ddb1ae9561dd5410: src/main.rs
-
-/Users/daiv/coding/learning/rust/projects/guessing_game/target/debug/deps/guessing_game-ddb1ae9561dd5410.d: src/main.rs
-
-src/main.rs:
