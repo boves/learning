b7d00dd058de2046006f575e313b7f1bd268691d
boves <daiv.boveri@gmail.com>
2022-04-26 22:48:30 -0500

    created data_types/

---
+++ rust/projects/data_types/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let x = 2.0; // f64
+    println!("x is: {}",x);
+
+    let y: f32 = 3.0; // f32
+    println!("y is: {}", y);
+    
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3; // results in 0
+
+    // remainder
+    let remainder = 43 % 5;
+
+    println!("difference is: {}", difference);
+    println!("product is: {}", product);
+    println!("quotient is: {}", quotient);
+    println!("floored is: {}", floored);
+    println!("remainder is: {}", remainder);
+}
