056e9d71f55c64a11a355ce119536c066a1417d7
boves <daiv.boveri@gmail.com>
2022-04-28 21:05:26 -0500

    Minor update

--- rust/projects/data_types/src/main.rs
+++ rust/projects/data_types/src/main.rs
@@ -1,16 +1,19 @@
 fn main() {
-    println!("**************************************************");
-    println!("\n\t\tdata_types/main.rs\n");
+    // let asterisks =  "**************************************************";
+    let asterisks =  "********************************************";
+                                                    // ************
+    println!("{}{}", asterisks, asterisks);
+    println!("\n\t\t\tdata_types/main.rs\n");
 
-    println!("\t\tFloating-Point Numbers");
+    println!("\t\t\tFloating-Point Numbers");
     let x = 2.0; // f64
-    println!("\tx is: {}",x);
+    println!("\t\tx is: {}",x);
 
     let y: f32 = 3.0; // f32
-    println!("\ty is: {}", y);
+    println!("\t\ty is: {}", y);
     println!("\n");
 
-    println!("\t\tNumeric Operations");
+    println!("\t\t\tNumeric Operations");
     // subtraction
     let difference = 95.5 - 4.3;
 
@@ -24,37 +27,42 @@ fn main() {
     // remainder
     let remainder = 43 % 5;
 
-    println!("\tdifference is: {}", difference);
-    println!("\tproduct is: {}", product);
-    println!("\tquotient is: {}", quotient);
-    println!("\tfloored is: {}", floored);
-    println!("\tremainder is: {}", remainder);
+    println!("\t\tdifference is: {}", difference);
+    println!("\t\tproduct is: {}", product);
+    println!("\t\tquotient is: {}", quotient);
+    println!("\t\tfloored is: {}", floored);
+    println!("\t\tremainder is: {}", remainder);
 
     println!("\n");
-    println!("\t\tThe Boolean Type\n");
+    println!("\t\t\tThe Boolean Type\n");
     
     let t = true; // implicit type annotation
     let f: bool = false; // with explicit type annotation
-    println!("\tImplicitly, 't' is: {}", t);
-    println!("\tExplicitly, 'f' is: {}", f);
+    println!("\t\tImplicitly, 't' is: {}", t);
+    println!("\t\tExplicitly, 'f' is: {}", f);
     println!("\n");
 
-    println!("\t\tThe Character Type");
+    println!("\t\t\tThe Character Type");
     let c = 'z';
     let z = 'ℤ';
     let heart_eyed_cat = '😻';
 
-    println!("\tc is: {}", c); 
-    println!("\tz is: {}", z); 
-    println!("\theart_eyed_cat is: {}\n", heart_eyed_cat); 
+    println!("\t\tc is: {}", c); 
+    println!("\t\tz is: {}", z); 
+    println!("\t\theart_eyed_cat is: {}\n", heart_eyed_cat); 
     
-    println!("\t\tCOMPOUND TYPES");
-    println!("\t\t    tuples");
+    println!("\t\t\tCOMPOUND TYPES");
+    println!("\t\t\t    tuples");
 
     let tup: (i32, f64, u8) = (500, 6.4, 1);
-    println!("\ttup is: {:?}", tup);
+    println!("\t\ttup is: {:?}", tup);
+    let (x, y, z) = tup;
+    
+    println!("\t\t - The value of x is: {}", x);
+    println!("\t\t - The value of y is: {}", y);
+    println!("\t\t - The value of z is: {}", z);
      
     // End of output
     println!("\n\n");
-    println!("**************************************************");
+    println!("{}", asterisks);
 }
