287f86292f25e7dcdfe016d48b797b7c51089f7e
boves <daiv.boveri@gmail.com>
2022-05-04 14:34:19 -0500

    Finished functions in /rust

--- rust/projects/functions/src/main.rs
+++ rust/projects/functions/src/main.rs
@@ -1,6 +1,19 @@
 fn main() {
     another_function(5);
     print_labeled_measurements(5, 'h');
+    
+    let y = {
+        let x = 3;
+        x +1
+    };
+    println!("The value of y is: {}", y);
+
+    let x = five();
+
+    println!("The value of x is: {}", x);
+
+    let z = plus_one(5);
+    println!("The value of z is: {}", z);
 }
 
 fn another_function(x: i32) {
@@ -10,4 +23,12 @@ fn another_function(x: i32) {
 
 fn  print_labeled_measurements(value: i32, unit_label: char) {
     println!("The measurement is: {}{}", value, unit_label);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
