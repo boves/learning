c0bf7089e803b5fc2b17413fd1a0f72266e6ba32
boves <daiv.boveri@gmail.com>
2022-05-07 13:01:54 -0500

    [In Progress] Solving ch3_homework part 1

--- rust/projects/ch3_homework/src/main.rs
+++ rust/projects/ch3_homework/src/main.rs
@@ -1,3 +1,17 @@
+// 1. Convert temperatures between Fahrenheit and Celsius.
+// 2. Generate the nth Fibonacci number.
+// 3. Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” taking advantage of the repetition in the song.
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let temp_scale = convert_tempt('c'
+
+}
+
+fn convert_temp (scale: char, temp: f64) -> char { // &str {
+    
+    if char == 'c' | 'C' {
+
+    }
 }
