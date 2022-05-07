2c1b5d777837269c38134291e010bb27e3791544
boves <daiv.boveri@gmail.com>
2022-05-06 23:32:38 -0500

    Started coding /branches

--- rust/projects/branches/src/main.rs
+++ rust/projects/branches/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 7;
+
+    if number  < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    let number2 = 3;
+
+    if number2 != 0{
+        println!("number2 was something other than zero");
+    }
+
+    let number3 = 6;
+
+    if number3 % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number3 % 3 == 0 {
+        println!("number3 is divisible by 3");
+    } else if number3 % 2 == 0{
+        println!("number3 is divisible by 2");
+    } else {
+        println!("number3 is not divisible by 4, 3, or 2");
+    }
 }
