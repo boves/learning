198875092154c6dae059b4ea5d45909ec4178cea
boves <daiv.boveri@gmail.com>
2022-05-11 22:06:17 -0500

    Minor update

--- rust/projects/ch3_homework/src/main.rs
+++ rust/projects/ch3_homework/src/main.rs
@@ -4,27 +4,24 @@
 
 
 fn main() {
-    
+   
+   println!(""); 
     // TODO: collect input for scale and temp
-    let scale = convert_temp('c'); //, 20);
+   println!("Input is c, so...");
+   convert_temp('c'); //, 20);
+   println!("Input is f, so...");
+   convert_temp('f'); //, 20);
 }
 
 fn convert_temp (scale: char){//, temp: f64) -> char { // &str {
    
 
     if scale == 'c'{
-        println!("Scale is Celsius");
+        println!(" -Scale is Celsius");
     }
-<<<<<<< HEAD
 
      else{
-         println!("The scale is Fahrenheit.");
+         println!(" -Scale is Fahrenheit.");
      }
 
-=======
-    else{
-         println!("The scale is Fahrenheit.");
-         return scale;
-    }
->>>>>>> a8ea4578150ba3eae845438dc00efc342729a362
 }
