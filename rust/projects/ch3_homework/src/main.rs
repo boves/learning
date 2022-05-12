fa03407f590be6aaa5b79472781b84b67259b871
boves <daiv.boveri@gmail.com>
2022-05-11 22:37:28 -0500

    Finished part 1 of ch3_homework

--- rust/projects/ch3_homework/src/main.rs
+++ rust/projects/ch3_homework/src/main.rs
@@ -4,24 +4,30 @@
 
 
 fn main() {
-   
-   println!(""); 
-    // TODO: collect input for scale and temp
-   println!("Input is c, so...");
-   convert_temp('c'); //, 20);
-   println!("Input is f, so...");
-   convert_temp('f'); //, 20);
+
+    println!("");
+
+    println!("CHAPTER 3 HOMEWORK"); 
+    println!("1. Convert temperatures between F and C"); 
+    convert_temp('c', 20); //, 20);
+    convert_temp('f', 212);
+    
 }
 
-fn convert_temp (scale: char){//, temp: f64) -> char { // &str {
+fn convert_temp (scale: char, temp: i32) {// -> char { // &str {
    
 
     if scale == 'c'{
-        println!(" -Scale is Celsius");
+        println!(" Scale is Celsius");
+        // println!("The temp is: {}°C", temp);
+        let temperature = (temp * 9/5) + 32;
+        println!("  -{}°C is: {}°F\n", temp,  temperature);
     }
 
      else{
-         println!(" -Scale is Fahrenheit.");
+         println!(" Scale is Fahrenheit.");
+         let temperature = (temp - 32) * 5/9;
+         println!("  -{}°F is: {}°C\n", temp, temperature); 
      }
 
 }
