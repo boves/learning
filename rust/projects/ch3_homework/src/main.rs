6ba472469406d1e48020813542c047559f43c535
boves <daiv.boveri@gmail.com>
2022-05-12 22:23:52 -0500

    Finished Part 2 of ch_3_homework; working on part 3

--- rust/projects/ch3_homework/src/main.rs
+++ rust/projects/ch3_homework/src/main.rs
@@ -12,6 +12,15 @@ fn main() {
     convert_temp('c', 20); //, 20);
     convert_temp('f', 212);
     
+    println!("*********************");
+    println!("2. Generate the nth Fibonacci number.");
+    let n = 10;
+    let result = f(n);
+
+    println!("The Fibonacci for n = {:?} is {:?}\n", n, result);
+    println!("*********************");
+
+    println!("3. Print the lyrics to the Christmas carol \"The Twelve Days of Christmas,\" taking advantage of the repetition in the song.");
 }
 
 fn convert_temp (scale: char, temp: i32) {// -> char { // &str {
@@ -30,4 +39,28 @@ fn convert_temp (scale: char, temp: i32) {// -> char { // &str {
          println!("  -{}°F is: {}°C\n", temp, temperature); 
      }
 
+}
+
+fn f(n: i32) -> i32 {
+    if n < 2{
+        return n;
+    } else {
+        return f(n-1) + f(n-2);
+    }
+}
+
+fn the_twelve_days_of_christmas() {
+On the twelfth day of Christmas, my true love sent to me
+
+verses = ["A partridge in a pear tree", "Two turtle doves, and", "Three french hens", 
+    "Four calling birds", "Five golden rings", "Seven swans a-swimming", 
+    "Six geese a-laying", "Eight maids a-milking",  "Nine ladies dancing", 
+    "Ten lords a-leaping", "Eleven pipers piping", "Twelve drummers drumming"]
+
+
+
+
+ordinals = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "twelfth"] 
+opening_line = "On the {:?} day of Christmas, my true love sent to me"
+    
 }
