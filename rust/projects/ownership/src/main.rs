e8539c3e85aa7189d97945470bd7dee901ff9b4f
boves <daiv.boveri@gmail.com>
2022-05-15 16:26:14 -0500

    ownership in process

--- rust/projects/ownership/src/main.rs
+++ rust/projects/ownership/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    let s = "hello";
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{}", s); // this will print 'hello, world!' 
+
+    let x = 5;
+    let y = x;
+
+    println!("x is {}, and y is {}", x, y);
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 is {}, s2 is {}", s1, s2);
 }                   
