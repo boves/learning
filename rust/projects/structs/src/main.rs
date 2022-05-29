cae750bc05e1bd9d1a14d86baf231be2e4c2654d
boves <daiv.boveri@gmail.com>
2022-05-28 21:30:47 -0500

    Working through structs/

--- rust/projects/structs/src/main.rs
+++ rust/projects/structs/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    
+    println!("user1.email is: {}", user1.email);
+    user1.email = String::from("anotheremail@example.com");
+    println!("user1.email is now: {}", user1.email);
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
