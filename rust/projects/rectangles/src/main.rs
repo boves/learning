dcaac1dafbfa196d598bb9a04af27d9438ea5733
boves <daiv.boveri@gmail.com>
2022-06-07 17:32:11 -0500

    Minor update: implementing width()

--- rust/projects/rectangles/src/main.rs
+++ rust/projects/rectangles/src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height    
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
 }
 
 fn main() {
@@ -16,9 +20,13 @@ fn main() {
         height: 50,
     };
 
+    if rect1.width(){
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
-
+    
 }
