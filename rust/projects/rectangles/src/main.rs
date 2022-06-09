9637e21e9554bcc6497a9e45ce0ec261556cf4a5
boves <daiv.boveri@gmail.com>
2022-06-08 23:04:13 -0500

    Minor update

--- rust/projects/rectangles/src/main.rs
+++ rust/projects/rectangles/src/main.rs
@@ -12,6 +12,10 @@ impl Rectangle {
     fn width(&self) -> bool {
         self.width > 0
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -28,5 +32,17 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
     
 }
