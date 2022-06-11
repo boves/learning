7eb95810b1650a5d5867b542f7d6e79bf912a443
boves <daiv.boveri@gmail.com>
2022-06-11 00:15:26 -0500

    First re-factor of IpAddr enum

--- rust/projects/enums/src/main.rs
+++ rust/projects/enums/src/main.rs
@@ -1,14 +1,10 @@
 enum IpAddrKind {
-    V4,
-    V6
+    V4(String),
+    V6(String),
 }
 
-fn main {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-
-    route(IpAddrKind::V4);
-    route(IpAddrKind::V6);
+fn main() {
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrKind::V6(String::from("::1"));
 }
 
-fn route(ip_kind: IpAddrKind){}
