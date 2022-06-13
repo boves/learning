000d5e8392a341cb2681c029428a3326634b541e
boves <daiv.boveri@gmail.com>
2022-06-12 23:59:02 -0500

    Learning enums: this version might not compile

--- rust/projects/enums/src/main.rs
+++ rust/projects/enums/src/main.rs
@@ -1,11 +1,17 @@
-enum IpAddrKind {
-    V4(u8, u8, u8, u8),
-    V6(String),
+enum Message {
+    Quit,
+    Move { x: i32, y: i32},
+    Write(string),
+    ChangeColor(i32, i32, i32),
+}
+impl Message {
+    fn call(&self) {
+        // method body
 }
 
 fn main() {
-    let home = IpAddrKind::V4(127, 0, 0, 1);
+    let m = Message:Write(String::from("hello"));
+    m.call();
 
-    let loopback = IpAddrKind::V6(String::from("::1"));
 }
 
