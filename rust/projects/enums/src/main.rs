b5282f76ac36273f75e6c3535063707c53785404
boves <daiv.boveri@gmail.com>
2022-06-16 13:25:17 -0500

    Minor update: adding matching to plus_one()

--- rust/projects/enums/src/main.rs
+++ rust/projects/enums/src/main.rs
@@ -17,12 +17,23 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25 {
+        Coin::Quarter(state) => {
             println!("State quarter from {:?}!", state);
+            25
         }
     }
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+        }
+}
+
 fn main () {
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 
 }
