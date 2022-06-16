2b46f6f209ccd5fd7372f16fc4718e4aee960f93
boves <daiv.boveri@gmail.com>
2022-06-15 19:24:24 -0500

    Minor update: implementing UsState for Quarters

--- rust/projects/enums/src/main.rs
+++ rust/projects/enums/src/main.rs
@@ -1,8 +1,15 @@
+#[derive(Debug)] // so we can inspect the state
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -10,9 +17,12 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter => 25 {
+            println!("State quarter from {:?}!", state);
+        }
     }
 }
 
 fn main () {
+
 }
