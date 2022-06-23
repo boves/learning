ee19bdf2f787f1a347789766bd2e97be1fe5da71
boves <daiv.boveri@gmail.com>
2022-06-23 16:14:15 -0500

    Minor update: I was calling cargo run instead of cargo build; who knowwwwwws

--- rust/projects/restaurant/src/lib.rs
+++ rust/projects/restaurant/src/lib.rs
@@ -7,7 +7,7 @@ mod front_of_house {
 
 pub fn eat_at_restaurant () {
     //Absolute path
-    crate::front_of_house::hosting:add_to_waitlist();
+    crate::front_of_house::hosting::add_to_waitlist();
 
     //relative path
     front_of_house::hosting::add_to_waitlist();
