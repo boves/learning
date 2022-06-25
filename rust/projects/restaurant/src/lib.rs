18b9bf276868b9eecf9424498eb95f349e332d39
boves <daiv.boveri@gmail.com>
2022-06-24 23:53:41 -0500

    implementing Breakfast struct

--- rust/projects/restaurant/src/lib.rs
+++ rust/projects/restaurant/src/lib.rs
@@ -23,4 +23,18 @@ mod back_of_house {
     }
 
     fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        season_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
 }
