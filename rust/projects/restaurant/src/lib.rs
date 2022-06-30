2ba5166dcd3d79e03deb140844a67c8a679fc02a
boves <daiv.boveri@gmail.com>
2022-06-29 22:02:40 -0500

    Minor update: using external packages

--- rust/projects/restaurant/src/lib.rs
+++ rust/projects/restaurant/src/lib.rs
@@ -56,3 +56,14 @@ mod back_of_house {
         Salad,
     }
 }
+
+use std::fmt;
+use std::io;
+
+fn function1() -> fmt::Result {
+    //--snip--
+}
+
+fn function2() -> io::Result {
+    // --snip--
+}
