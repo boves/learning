d94d4e8729fc0376fb6a154ca2d8412ad1b727ca
boves <daiv.boveri@gmail.com>
2022-06-29 22:06:01 -0500

    Minor update: providing new names with the 'as' keyword

--- rust/projects/restaurant/src/lib.rs
+++ rust/projects/restaurant/src/lib.rs
@@ -57,13 +57,15 @@ mod back_of_house {
     }
 }
 
-use std::fmt;
-use std::io;
+use std::fmt::Result;
+use std::io::Result as IoResult;
 
-fn function1() -> fmt::Result {
+fn function1() -> Result {
     //--snip--
 }
 
-fn function2() -> io::Result {
+fn function2() -> IoResult {
     // --snip--
 }
+
+
