efa24ea7f9abeb88f81d20e00734c105d3879f65
boves <daiv.boveri@gmail.com>
2022-07-02 18:31:54 -0500

    Referencing values in a vec<T>

--- rust/projects/collections/src/main.rs
+++ rust/projects/collections/src/main.rs
@@ -12,14 +12,28 @@ fn main() {
 
     println!("{:?}", v3);
 
-    let v = vec![1, 2, 3, 4, 5];
-    let third: &i32 = &v[2];
+    let v4 = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v4[2];
     println!("The third element is {}", third);
 
-    match.v.get(2) {
-        Some(third) => println!("The third element of {}", third),
+    match v4.get(2) {
+        Some(third) => println!("The third element is {}", third),
         None => println!("There is no third element."),
     }
+
+    let v5 = vec![1, 2, 3, 4, 5];
+
+    // let does_not_exist = &v5[100];
+    let does_not_exist = v5.get(100);
+
+    let mut v6 = vec![1, 2, 3, 4, 5];
+    let first = &v6[0];
+
+    v6.push(6);
+
+    // println!("The first element is: {}", first);
+    println!("The first element is: {:?}", &v6[0]);
+
 
 
 
