25fa2aef7b037c07f5f9e29be67b7aec166c2b0f
boves <daiv.boveri@gmail.com>
2022-07-04 13:29:28 -0500

    Final commit for Vector collection tutorial

--- rust/projects/collections/src/main.rs
+++ rust/projects/collections/src/main.rs
@@ -42,14 +42,23 @@ fn main() {
 
     }
 
-
-
     let mut v = vec![100, 32, 57];
     for i in &mut v {
         *i += 50;
         println!("--{:?}", i);
     }
 
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }  
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 
 
     // do stuff with  v2
