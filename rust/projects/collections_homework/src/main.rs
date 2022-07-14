06cc800d2f3842031ec75df19e915bf14fe4578a
boves <daiv.boveri@gmail.com>
2022-07-14 12:30:53 -0500

    Finished collections_homework part 1: computing median, mode; added computing average for extra credit

--- rust/projects/collections_homework/src/main.rs
+++ rust/projects/collections_homework/src/main.rs
@@ -1,39 +1,25 @@
+use std::collections::HashMap;
+
 fn main() { 
     // 1. Given a list of integers, use a vector and return the median
     // (when sorted, the value in the middle position) and mode (the value
     // that occurs most often; a hash map will be helpful here) of the list.
 
-    use std::collections::HashMap;
+    
     let mut list = vec![60, 60, 80, 90, 10, 20, 30, 40, 50];
 
-    // sort the vector
-    list.sort();
-    assert_eq!(list, vec![10, 20, 30, 40, 50, 60, 60, 80, 90]);
+    let avg = calculate_average(&list);
+    let median = calculate_median(&list);
+    let mode = calculate_mode(&list);
 
-    // count the vector
-    let count = list.len();
 
-    let median = list[4];
-    println!("The median is {:?}", median);
-
-    println!("I don't know how to sum a vector");
     println!("Fam's in from Ireland and I'm lazy, so this is all I get today.");
 
-    let mut map = HashMap::new();
-
-    for i in list {
-        let count = map.entry(i).or_insert(0);
-        *count += 1;
-
-    }
-
-    let mode = String::from("Temporary");
-
-    println!("map is : {:?}", map);
-
-    println!("The mode is: {:?}", mode);
-    println!("Well, today I created a branch. I'm going to merge it later or re-write it here");
+    println!("Solution to problem 1");
     
+    println!("avg is: {:?}", avg); 
+    println!("median is: {:?}", median); 
+    println!("mode is: {:?}", mode); 
 
 
     // 2. Convert strings to pig latin. The first consonant of each word is
@@ -50,4 +36,49 @@ fn main() {
     // the user retrieve a list of all people in a department or all people
     // in the company by department, sorted alphabetically.
 
+}
+
+
+fn calculate_average(nums: &Vec<i32>) -> f32 {
+    let mut sum = 0;
+    for x in nums {
+        sum += x;
+    }
+
+    sum as f32 / nums.len() as f32
+}
+
+fn calculate_median(nums: &Vec<i32>) -> f32 {
+    let mut sorted = nums.clone();
+    sorted.sort();
+
+    let mid = sorted.len() / 2;
+    if sorted.len() % 2 == 0 {
+        let x = sorted[mid] as f32;
+        let y = sorted[mid-1] as f32;
+        return (x + y) * 0.5;
+    }
+
+    sorted[mid] as f32
+}
+
+fn calculate_mode(nums: &Vec<i32>) -> i32 {
+    let mut times = HashMap::new();
+    for x in nums {
+        let count = times.entry(x).or_insert(0);
+        *count += 1
+    }
+
+    let mut best_key = nums[0]; {
+
+    }
+    let mut best_val = times.get(&best_key).unwrap();
+
+    for (key, value) in &times {
+        if value > best_val {
+            best_key = **key;
+            best_val = value;
+        }
+    }
+    best_key
 }
