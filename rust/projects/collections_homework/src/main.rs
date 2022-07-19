fb5cf77768fba6c2444d7254c0020f0e72f14376
boves <boves@users.noreply.github.com>
2022-07-18 20:41:45 -0500

    Starting text interface

--- rust/projects/collections_homework/src/main.rs
+++ rust/projects/collections_homework/src/main.rs
@@ -47,13 +47,16 @@ fn main() {
 
     println!("The pig-latin for {:?} is {:?}",tx, px);
 
-    
+   println!("\n\n"); 
 
     // 3. Using a hash map and vectors, create a text interface to allow a
     // user to add employee names to a department in a company. For
     // example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let
     // the user retrieve a list of all people in a department or all people
     // in the company by department, sorted alphabetically.
+
+    println!("EMPLOYEE ROSTER INTERFACE");
+    println!("What do you want to do? ")
 
 }
 
