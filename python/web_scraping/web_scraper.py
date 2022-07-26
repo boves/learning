df06f6f17ff022e03884c85349e977ca0ee82b79
boves <daiv.boveri@gmail.com>
2022-07-26 13:34:25 -0500

    Update web_scraper.py

--- python/web_scraping/web_scraper.py
+++ python/web_scraping/web_scraper.py
@@ -145,10 +145,18 @@ form.select("input")[1]["value"] = "ThunderDude"
 profiles_page = browser.submit(form, login_page.url)
 
 print(profiles_page.url)
+links = profiles_page.soup.select("a")
 
+for link in links:
+		address = link["href"]
+		text = link.text
+		print(f"{text}: {address}")
 
-
-
+base_url = "http://olympus.realpython.org"
+for link in links:
+	address = base_url + link["href"]
+	text = link.text
+	print(f"{text}: {address}")
 
 
 
