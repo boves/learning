cdfa5e5772121fa2aa1644e604f514ef15a3f9cd
boves <daiv.boveri@gmail.com>
2022-07-27 12:13:25 -0500

    Finishing web scraper

--- python/web_scraping/web_scraper.py
+++ python/web_scraping/web_scraper.py
@@ -158,5 +158,20 @@ for link in links:
 	text = link.text
 	print(f"{text}: {address}")
 
+browser = mechanicalsoup.Browser()
+page = browser.get("http://olympus.realpython.org/dice")
+tag = page.soup.select("#result")[0]
+result = tag.text
+
+print(f"The result of your dice roll is: {result}")
+
+import time
+for i in range(4):
+	page = browser.get("http://olympus.realpython.org/dice")
+	tag = page.soup.select("#result")[0]
+	result = tag.text 
+	print(f"The result of your dice roll is: {result}")
+
+
 
 
