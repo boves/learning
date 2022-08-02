504b47cbbba00cc9f805c7fe65b9a4f4fbf72710
boves <daiv.boveri@gmail.com>
2022-08-01 20:58:22 -0500

    Propagating errors 4

--- rust/projects/error-handling/src/main.rs
+++ rust/projects/error-handling/src/main.rs
@@ -1,15 +1,11 @@
 #![allow(unused)]
 fn main() {
-    use std::fs::File;
-    use std::io::{self, Read};
+    use std::fs;
+    use std::io;
     
 
     fn read_username_from_file() -> Result<String, io::Error> {
-        let mut s = String::new();
-
-        File::open("hello.txt")?.read_to_string(&mut s)?;
-
-        Ok(s)
+        fs::read_to_string("hello.txt")
     }
     
 }
