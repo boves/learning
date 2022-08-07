46fd82c7b093705a730a5537dcd6a1cba233ccc7
boves <daiv.boveri@gmail.com>
2022-08-07 15:03:18 -0500

    Learning how to use generic types in structs

--- rust/projects/chapter10/src/main.rs
+++ rust/projects/chapter10/src/main.rs
@@ -1,33 +1,10 @@
-fn largest_i32(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
+#![allow(unused_variables)]
+#![allow(dead_code)]
+struct Point<T> {
+    x: T,
+    y: T,
 }
-
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
-}
-
-fn main(){
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest_i32(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest_char(&char_list);
-    println!("The largest char is {}", result);
+fn main() {
+    let integer = Point {x: 5, y: 10};
+    let float = Point{ x: 1.0, y: 4.0};
 }
