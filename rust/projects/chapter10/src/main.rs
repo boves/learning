fc876cf2cf3f00a4be1f72188f58b55f5bd2a2ad
boves <daiv.boveri@gmail.com>
2022-08-06 21:01:20 -0500

    main function for generic types in function definitions

--- rust/projects/chapter10/src/main.rs
+++ rust/projects/chapter10/src/main.rs
@@ -20,3 +20,14 @@ fn largest_char(list: &[char]) -> char {
     largest
 }
 
+fn main(){
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
+}
