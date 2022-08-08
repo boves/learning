a93b4d2a3dd19b8e971d6ba160c6606267deff11
boves <daiv.boveri@gmail.com>
2022-08-08 13:03:07 -0500

    Generic parameters defined within function that aren't declared at top of impl definition

--- rust/projects/chapter10/src/main.rs
+++ rust/projects/chapter10/src/main.rs
@@ -1,40 +1,26 @@
 #![allow(dead_code)]
 
-// Struct with generic types
-struct Point<T> {
-    x: T,
-    y: T,
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
 }
 
-// Implementation of Point for generic types
-// Requires impl<T> to indicate to the compiler that
-// the the type being implemented is generic
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
-}
-
-// Implementation of Point for only f32
-// Lack of <T> after impl indicates that
-// the following definition is specific.
-impl Point<f32> {
-
-    // This function isn't available for
-    // all data types, only f32
-    fn distance_from_origin(&self) -> f32 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
-    }
-
 }
 
 fn main() {
-    let p = Point { x: 5, y: 10 };
+    let p1 = Point { x: 5, y: 10.4};
+    let p2 = Point { x: "Hello", y: 'c'};
 
-    println!("p.x = {}", p.x());
+    let p3 = p1.mixup(p2);
 
-    let p = Point { x: 5.0, y: 10.0};
-    let dis = p.distance_from_origin();
+   println!("p3.x = {}, p3.y = {}", p3.x, p3.y); 
 
-    println!("Dis is: {:?}", dis);
 }
+
