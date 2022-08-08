46d6eb3ddecc31f0510b1f132e32a6fb8c86b202
boves <daiv.boveri@gmail.com>
2022-08-08 12:49:58 -0500

    Implementing a function definition for f32 inheriting off a generic type

--- rust/projects/chapter10/src/main.rs
+++ rust/projects/chapter10/src/main.rs
@@ -1,18 +1,40 @@
-#![allow(unused_variables)]
-#![allow(dead_code )]
+#![allow(dead_code)]
 
-struct Point<T, U> {
+// Struct with generic types
+struct Point<T> {
     x: T,
-    y: U,
-}
-enum Result<T, E>{
-    Ok(T),
-    Err(E),
+    y: T,
 }
 
+// Implementation of Point for generic types
+// Requires impl<T> to indicate to the compiler that
+// the the type being implemented is generic
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+// Implementation of Point for only f32
+// Lack of <T> after impl indicates that
+// the following definition is specific.
+impl Point<f32> {
+
+    // This function isn't available for
+    // all data types, only f32
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+
+}
 
 fn main() {
-    let both_integer = Point {x: 5, y: 10};
-    let both_float = Point{ x: 1.0, y: 4.0};
-    let integer_and_float = Point {x: 5, y: 4.0};
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {}", p.x());
+
+    let p = Point { x: 5.0, y: 10.0};
+    let dis = p.distance_from_origin();
+
+    println!("Dis is: {:?}", dis);
 }
