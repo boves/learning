43b6c2d31923829fd4a72bda2b87ba2a6075ba26
boves <daiv.boveri@gmail.com>
2022-08-09 14:15:57 -0500

    Validating references with lifetimes

--- rust/projects/chapter10/src/main.rs
+++ rust/projects/chapter10/src/main.rs
@@ -1,26 +1,26 @@
 #![allow(dead_code)]
 
-struct Point<X1, Y1> {
-    x: X1,
-    y: Y1,
-}
+fn main() {
+    {
+        let string1 = String::from("abcd");
+        let string2 = "xyz";
 
-impl<X1, Y1> Point<X1, Y1> {
-    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
-        Point {
-            x: self.x,
-            y: other.y,
-        }
+        let result = longest(string1.as_str(), string2);
+        println!("The longest string is {}", result);
+    }
+
+    let string1 = String::from("long string is long");
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
     }
 }
 
-fn main() {
-    let p1 = Point { x: 5, y: 10.4};
-    let p2 = Point { x: "Hello", y: 'c'};
-
-    let p3 = p1.mixup(p2);
-
-   println!("p3.x = {}, p3.y = {}", p3.x, p3.y); 
-
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str{
+    if x.len() > y.len(){
+        x
+    } else {
+        y
+    }
 }
-
