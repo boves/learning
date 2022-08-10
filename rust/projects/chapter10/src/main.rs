c7203db1c6e764245e1bbe66fabc7c62e30215a8
boves <daiv.boveri@gmail.com>
2022-08-10 11:29:26 -0500

    Lifetime annotations in struct definitions and implementations

--- rust/projects/chapter10/src/main.rs
+++ rust/projects/chapter10/src/main.rs
@@ -1,26 +1,37 @@
 #![allow(dead_code)]
 
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a>{
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str{
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+
+}
+
 fn main() {
-    {
-        let string1 = String::from("abcd");
-        let string2 = "xyz";
-
-        let result = longest(string1.as_str(), string2);
-        println!("The longest string is {}", result);
-    }
-
-    let string1 = String::from("long string is long");
-    {
-        let string2 = String::from("xyz");
-        let result = longest(string1.as_str(), string2.as_str());
-        println!("The longest string is {}", result);
-    }
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let _i = ImportantExcerpt {
+        part: first_sentence
+    };
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str{
-    if x.len() > y.len(){
-        x
-    } else {
-        y
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate(){
+        if item == b' ' {
+            return &s[0..i];
+        }
     }
+    &s[..]
 }
+
