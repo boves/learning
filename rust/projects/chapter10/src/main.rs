520e01ab172b2440aefbb95eae9b1ad300d1651e
boves <daiv.boveri@gmail.com>
2022-08-13 12:54:24 -0500

    Generic Type Paramters, Trait Bounds, and Lifetimes Together

--- rust/projects/chapter10/src/main.rs
+++ rust/projects/chapter10/src/main.rs
@@ -1,43 +1,18 @@
 #![allow(dead_code)]
+use std::fmt::Display;
 
-struct ImportantExcerpt<'a> {
-    part: &'a str,
-}
-
-impl<'a> ImportantExcerpt<'a>{
-    fn level(&self) -> i32 {
-        3
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement: {}", ann);
+    if x.len() > y.len(){
+        x
+    } else {
+        y
     }
-
-    fn announce_and_return_part(&self, announcement: &str) -> &str{
-        println!("Attention please: {}", announcement);
-        self.part
-    }
-
 }
-
-fn main() {
-    let novel = String::from("Call me Ishmael. Some years ago...");
-    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
-    let _i = ImportantExcerpt {
-        part: first_sentence
-    };
-
-    // static variables are encoded directly in the binary
-    // so are always available
-    let _s: &'static str = "I have a static lifetime.";
-
-    println!("Saturday August 12 commit");
-}
-
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate(){
-        if item == b' ' {
-            return &s[0..i];
-        }
-    }
-    &s[..] // I thought I wrote this code on Aug 12, but I didn't
-}
-
