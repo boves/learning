b2f7a54e4611adb6743556f31c0acf4d109a1bcb
boves <daiv.boveri@gmail.com>
2022-09-01 16:43:09 -0500

    Implementing the search_case_insensitive Function

--- rust/projects/minigrep/src/lib.rs
+++ rust/projects/minigrep/src/lib.rs
@@ -42,6 +42,23 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -52,11 +69,23 @@ mod tests {
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
-}
 
-// Testing to see if my account can still authenticate
-// because I had a problem in another repository
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
