1648b86340f5616169a1b6f89168eafdf8ca635b
boves <daiv.boveri@gmail.com>
2022-09-16 14:53:23 -0500

    Enabling recursive types with boxes- attempt one doesn't compile

--- rust/projects/chapter15/cons-list/src/main.rs
+++ rust/projects/chapter15/cons-list/src/main.rs
@@ -1,5 +1,15 @@
+use crate::List::{Cons, Nil};
+
+enum List {
+    Cons(i32, List),
+    Nil,
+}
+
 fn main() {
     println!("Using a Box<t>");
     let b = Box::new(5);
     println!("b = {}", b);
+
+    let list = Cons(1, Cons(2, Cons(3, Nil)));
 }
+
