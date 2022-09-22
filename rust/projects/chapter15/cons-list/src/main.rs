397becbe404ccf15aadf0b0c4e92657914ad7ac3
boves <daiv.boveri@gmail.com>
2022-09-22 13:57:46 -0500

    Rc<T>, the reference counted smart pointer

--- rust/projects/chapter15/cons-list/src/main.rs
+++ rust/projects/chapter15/cons-list/src/main.rs
@@ -1,14 +1,13 @@
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
 use crate::List::{Cons, Nil};
+use std::rc::Rc;
 
 fn main() {
-    println!("Using a Box<t>");
-    let b = Box::new(5);
-    println!("b = {}", b);
-
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }
