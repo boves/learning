833e7906bfa597b8fc147bc36e237262f54f5ef9
boves <daiv.boveri@gmail.com>
2022-10-23 15:36:11 -0500

    Matching multiple patterns and matching ranges of values with ..=

--- rust/projects/chapter18/patterns/src/main.rs
+++ rust/projects/chapter18/patterns/src/main.rs
@@ -40,6 +40,7 @@ fn main() {
         println!("{}", x);
     }; 
 
+    // Matching named variables
     let x = 1;
 
     match x { 
@@ -59,7 +60,31 @@ fn main() {
     }
 
     println!("at the end: x = {:?}, y = {y}", x);
-    
+
+    // Multiple patterns
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    // Matching ranges of values with ..=
+
+    let x = 5;
+
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
