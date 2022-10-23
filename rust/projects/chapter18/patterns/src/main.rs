43f1a1fc87d013da287fa33ed26c57b8fe4899e3
boves <daiv.boveri@gmail.com>
2022-10-22 22:43:03 -0500

    Matching named variables

--- rust/projects/chapter18/patterns/src/main.rs
+++ rust/projects/chapter18/patterns/src/main.rs
@@ -48,6 +48,17 @@ fn main() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+
+    let x = Some (5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", x),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {y}", x);
     
 }
 
