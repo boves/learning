45176679e2bb8fa4635464df7c5ccdb863fc7171
boves <daiv.boveri@gmail.com>
2022-10-25 13:12:10 -0500

    Destructure enums

--- rust/projects/chapter18/patterns/src/main.rs
+++ rust/projects/chapter18/patterns/src/main.rs
@@ -3,6 +3,13 @@ struct Point {
     y: i32,
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 fn main() {
     let favorite_color: Option<&str> = None;
     let is_tuesday = false;
@@ -101,7 +108,25 @@ fn main() {
     match p {
         Point { x, y:0 } => println!(" {}", x),
         Point { x: 0, y } => println!("On the x axis at {}", y),
-        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+            Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+
+    // Destructuring enums
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!(
+                "Move in the x direction {} and in the y direction {}", x, y
+            );
+        }
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => println!(
+            "Change the color to red {}, green {}, and blue {}", r, g, b
+        ),
     }
 }
 
