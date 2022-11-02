7b0c8189826c83fbe0435a41e02d3768b6546aef
boves <daiv.boveri@gmail.com>
2022-11-01 22:20:43 -0500

    Extra conditionals with Match Guards

--- rust/projects/chapter18/patterns/src/main.rs
+++ rust/projects/chapter18/patterns/src/main.rs
@@ -195,6 +195,16 @@ fn main() {
         Point2 { x, .. } => println!("x is {}", x),
     }
 
+    // Extra conditionals with Match Guards
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+        Some(x) => println!("The number {} is odd", x),
+        None => (),
+    }
+
+
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
