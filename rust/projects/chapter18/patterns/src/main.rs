d4e96f71057a7133ea644d6da549d03a0dd7bd9f
boves <daiv.boveri@gmail.com>
2022-11-04 22:13:56 -0500

    Multiple patterns

--- rust/projects/chapter18/patterns/src/main.rs
+++ rust/projects/chapter18/patterns/src/main.rs
@@ -216,6 +216,15 @@ fn main() {
 
     println!("at the end: x = {:?}, y = {y}", x);
 
+    // Multiple Patterns
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
 
 }
 
