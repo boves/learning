a1b16a3e4e96f9271beb6b140500d3b7169b8892
boves <daiv.boveri@gmail.com>
2022-11-16 23:54:47 -0600

    using an unsafe block, a raw pointer, and some calls to unsafe functions

--- rust/projects/chapter19/unsafe-example/src/main.rs
+++ rust/projects/chapter19/unsafe-example/src/main.rs
@@ -1,3 +1,5 @@
+use std::slice;
+
 fn main() {
     let address = 0x012345usize;
     let r = address as *const i32;
@@ -27,5 +29,22 @@ fn main() {
 
     assert_eq!(a, &mut [1, 2, 3]);
     assert_eq!(b, &mut [4, 5, 6]);
+
+
+
+
 }
 
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]){
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
