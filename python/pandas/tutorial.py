b90a9c981abce0b6afa18441f0e1c5cd32201674
Daiv B. of the Shadow Hierarchy <boves@users.noreply.github.com>
2022-11-18 05:53:14 +0000

    Organizing Python; created new Pandas dir.

---
+++ python/pandas/tutorial.py
@@ -0,0 +1,7 @@
+# working through: https://pandas.pydata.org/pandas-docs/stable/getting_started/intro_tutorials/02_read_write.html
+import pandas as pd
+
+url = "https://raw.githubusercontent.com/pandas-dev/pandas/main/doc/data/titanic.csv"
+titanic = pd.read_csv(url)
+
+titanic.head(8)
