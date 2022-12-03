6a8720cdfbf777a7f85998c8e931a827db69d31e
boves <daiv.boveri@gmail.com>
2022-12-02 22:12:50 -0600

    Creating my first plot in pandas! Fixed reference error

--- python/pandas/getting_started_2.py
+++ python/pandas/getting_started_2.py
@@ -1,3 +1,6 @@
 import pandas as pd # URL= https://pandas.pydata.org/pandas-docs/stable/getting_started/intro_tutorials/04_plotting.html
 import matplotlib.pyplot as plt
-air_quality = pd.read_csv("air_quality_no2.csv", index_col=0, parse_dates=True)
+air_quality = pd.read_csv("data/air_quality_no2.csv", index_col=0, parse_dates=True) # os.chdir(r"./pandas/) if this doesn't work
+air_quality.head()
+air_quality.plot()
+plt.show()
