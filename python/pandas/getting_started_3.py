417144ccaac002c61b3da5f6df2fbb3af9bec040
boves <daiv.boveri@gmail.com>
2022-12-07 23:59:12 -0600

    Minor update

--- python/pandas/getting_started_3.py
+++ python/pandas/getting_started_3.py
@@ -1,5 +1,9 @@
-import pandas as pd # URL= https://pandas.pydata.org/pandas-docs/stable/getting_started/intro_tutorials/04_plotting.html
+import pandas as pd # URL= https://pandas.pydata.org/pandas-docs/stable/getting_started/intro_tutorials/05_add_columns.html
 air_quality = pd.read_csv("data/air_quality_no2.csv", index_col=0, parse_dates=True) # os.chdir(r"./pandas/") if this doesn't work
 air_quality.head()
 air_quality["london_mg_per_cubic"] = air_quality["station_london"] * 1.882
+air_quality.head()
+air_quality["ratio_paris_antwerp"] = (
+    air_quality["station_paris"] / air_quality["station_antwerp"]
+)
 air_quality.head()
