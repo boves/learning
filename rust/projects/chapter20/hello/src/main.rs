bd2fafdab4278e7faf4e8d8dc0d3145bc0498e5c
boves <daiv.boveri@gmail.com>
2022-12-27 12:08:56 -0600

    validing the request and selectively responding

--- rust/projects/chapter20/hello/src/main.rs
+++ rust/projects/chapter20/hello/src/main.rs
@@ -21,18 +21,30 @@ fn handle_connection(mut stream: TcpStream) {
 
     if request_line == "GET / HTTP/1.1" {
         let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
+
+        let response = format! (
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        // some other request if needed
     }
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_strong("hello.html").unwrap();
-    let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    { // not sure i need this anymore but just making sure
+        // let http_request: Vec<_> = buf_reader
+        //     .lines()
+        //     .map(|result| result.unwrap())
+        //     .take_while(|line| !line.is_empty())
+        //     .collect();
+        // let status_line = "HTTP/1.1 200 OK";
+        // let contents = fs::read_to_string("hello.html").unwrap();
+        // let length = contents.len();
 
-    stream.write_all(response.as_bytes()).unwrap();
+        // let response =
+        //     format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        // stream.write_all(response.as_bytes()).unwrap();
+    }
 }
