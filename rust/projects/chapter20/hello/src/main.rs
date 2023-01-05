2162a861e48859931a1e472f75077281dcf2dd7c
boves <daiv.boveri@gmail.com>
2023-01-04 18:50:24 -0600

    Fixed compile errors

--- rust/projects/chapter20/hello/src/main.rs
+++ rust/projects/chapter20/hello/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         
-        thread::spanw(|| {
+        thread::spawn(|| {
             handle_connection(stream);
         });
     }
@@ -20,12 +20,13 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().lines.next.unwrap();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
     let (status_line, filename) = match &request_line[..]{
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        "GET /sleep HTTP/1.1" = > {
+        "GET /sleep HTTP/1.1" => {
             thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
         }
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
