9ad1eb212a89c5b77217099f497ce7a44837a8a9
boves <daiv.boveri@gmail.com>
2023-02-10 23:58:42 -0600

    Task: Setting up the data model for TodosList

--- react/react-todo-app/src/components/TodoContainer.js
+++ react/react-todo-app/src/components/TodoContainer.js
@@ -1,11 +1,12 @@
 import React from "react"
+import TodosList from "./TodosList";
+
 class TodoContainer extends React.Component {
     render () {
         return (
-            <React.Fragment>
-                <h1>Hello from Create React App</h1>
-                <p>I am in a React Component!</p>
-            </React.Fragment>
+            <div>
+                <TodosList todos={this.state.todos} />
+            </div> 
         )
     }
 }
