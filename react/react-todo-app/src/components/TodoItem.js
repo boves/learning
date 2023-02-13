473ec3f5831e9d0b645190d90d24b6d2b42e48fb
boves <daiv.boveri@gmail.com>
2023-02-12 18:42:56 -0600

    Task: adding onChange to TodoItem component

--- react/react-todo-app/src/components/TodoItem.js
+++ react/react-todo-app/src/components/TodoItem.js
@@ -3,7 +3,11 @@ import React from "react"
 const TodoItem = (props) => {
     return (
         <li>
-            <input type="checkbox" checked={props.todo.completed} /> {props.todo.title}
+            <input 
+                type="checkbox" 
+                checked={props.todo.completed}
+                onChange={() => console.log("clicked")}
+            /> {props.todo.title}
         </li>
     )
 }
