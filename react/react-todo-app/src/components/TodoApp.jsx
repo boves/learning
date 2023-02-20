19f82c6e42f70350baa3d32118f1936275d24069
boves <daiv.boveri@gmail.com>
2023-02-19 23:28:55 -0600

    Task: Rendering Todos data in the UI

--- react/react-todo-app/src/components/TodoApp.jsx
+++ react/react-todo-app/src/components/TodoApp.jsx
@@ -1,9 +1,13 @@
+import React from 'react';
+import Header from '@/components/Header'
+import TodosLogic from '@/components/TodosLogic'
+
 const TodoApp = () => {
     return (
-        <div>
-            <h1>Hello world!</h1>
-            <p>I am in a React Component</p>
-        </div>
+        <React.Fragment>
+            <Header />
+            <TodosLogic />
+        </React.Fragment>
     );
 };
 export default TodoApp;
