3dd0be818a8285f4852d5639668785fb57d364a3
boves <daiv.boveri@gmail.com>
2023-02-24 19:05:59 -0600

    Task: adding editable todo items

--- react/react-todo-app/src/components/TodoItem.jsx
+++ react/react-todo-app/src/components/TodoItem.jsx
@@ -1,11 +1,16 @@
 import styles from '@/styles/TodoItem.module.css';
+import { useState } from 'react';
 const TodoItem = ({ itemProp, handleChange, delTodo }) => {
+    const [editing, setEditing] = useState(false);
     const completedStyle = {
         fontStyle: 'italic',
         color: '#595959',
         opacity: 0.4,
         textDecoration: 'line-through',
       };
+    const handleEditing = () => {
+        setEditing(true);
+    };
     return (
         <li className={styles.item}>
             <div className={styles.content}>
@@ -14,11 +19,17 @@ const TodoItem = ({ itemProp, handleChange, delTodo }) => {
                     checked={itemProp.completed}
                     onChange={() => handleChange(itemProp.id)}
                 />
+                <button onClick={handleEditing}>Edit</button>
                 <button onClick={() => delTodo(itemProp.id)}>Delete</button>
                 <span style={itemProp.completed ? completedStyle: null}>
                     {itemProp.title}
                 </span>
             </div>
+            <input
+                type="text"
+                value={itemProp.title}
+                className={styles.textInput}
+            />
         </li>
     );
 };
