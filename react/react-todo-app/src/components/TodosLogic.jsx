f94c7da53888cad1d10758edca1eaf4b172dcf77
boves <daiv.boveri@gmail.com>
2023-03-01 16:52:42 -0600

    Task: Retrieving todos from local storage
    The static data has been removed. It should persist in local
    storage!

--- react/react-todo-app/src/components/TodosLogic.jsx
+++ react/react-todo-app/src/components/TodosLogic.jsx
@@ -4,23 +4,7 @@ import { useState, useEffect } from 'react'
 import { v4 as uuidv4 } from "uuid";
 
 const TodosLogic = () => {
-    const [todos, setTodos] = useState([
-        {
-          id: uuidv4(),
-          title: 'Setup development environment',
-          completed: true,
-        },
-        {
-          id: uuidv4(),
-          title: 'Develop website and add content',
-          completed: false,
-        },
-        {
-          id: uuidv4(),
-          title: 'Deploy to live server',
-          completed: false,
-        },
-      ]);
+    const [todos, setTodos] = useState(getInitialTodos());
       const handleChange = (id) => {
         console.log('clicked', id);
         setTodos((prevState) => 
@@ -61,6 +45,13 @@ const TodosLogic = () => {
         })
       );
     };
+
+    function getInitialTodos() {
+      // getting stored items
+      const temp = localStorage.getItem('todos');
+      const savedTodos = JSON.parse(temp);
+      return savedTodos || [];
+    }
     useEffect(() => {
       // storing todos items
       const temp = JSON.stringify(todos);
