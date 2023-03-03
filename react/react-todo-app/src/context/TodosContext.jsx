014799cbba67730b6db43a322b7982d9f86a9440
boves <daiv.boveri@gmail.com>
2023-03-03 15:12:01 -0600

    Task: Setting up the context store
    Set up TodosContext in src/context. Still need to link up to the
    interface and set wrap components with context provider.

---
+++ react/react-todo-app/src/context/TodosContext.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+const TodosContext = createContext(null);
+
+export const TodosProvider = ({ children }) => {
+    return (
+        <TodosContext.Provider value={'todos data'}>
+            {children}
+        </TodosContext.Provider>
+
+    );
+};
+export { TodosContext };
