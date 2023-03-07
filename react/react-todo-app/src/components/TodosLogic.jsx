86655d459e61346a9a0e2e72658fe241313ddba5
boves <daiv.boveri@gmail.com>
2023-03-07 17:44:53 -0600

    Task: Moving todos global data to context store
    This step is supposed to break the app. I will fix it in the
    next commit.

--- react/react-todo-app/src/components/TodosLogic.jsx
+++ react/react-todo-app/src/components/TodosLogic.jsx
@@ -1,69 +1,9 @@
 import InputTodo from "@/components/InputTodo";
 import TodosList from "@/components/TodosList";
-import { 
-  useState, 
-  useEffect,
-  createContext,
-  useContext,
-} from 'react';
-import { v4 as uuidv4 } from "uuid";
+
 import { TodosProvider } from "@/context/TodosContext";
 
 const TodosLogic = () => {
-    const [todos, setTodos] = useState(getInitialTodos());
-      const handleChange = (id) => {
-        console.log('clicked', id);
-        setTodos((prevState) => 
-            prevState.map((todo) => {
-                if (todo.id === id) {
-                    return {
-                        ...todo,
-                        completed: !todo.completed,
-                    };
-                }
-                return todo;
-            })
-        );
-    };
-    const delTodo =(id) => {
-      console.log('deleted', id);
-      setTodos([
-        ...todos.filter((todo) => {
-          return todo.id !== id;
-        }),
-      ]);
-    };
-    const addTodoItem = (title) => {
-      const newTodo = {
-        id: uuidv4(),
-        title: title,
-        completed: false,
-      };
-      setTodos([...todos, newTodo]);
-    }; 
-    const setUpdate = (updatedTitle, id) => {
-      setTodos(
-        todos.map((todo) => {
-          if (todo.id === id) {
-            todo.title = updatedTitle;
-          }
-          return todo;
-        })
-      );
-    };
-
-    function getInitialTodos() {
-      // getting stored items
-      const temp = localStorage.getItem('todos');
-      const savedTodos = JSON.parse(temp);
-      return savedTodos || [];
-    }
-    useEffect(() => {
-      // storing todos items
-      const temp = JSON.stringify(todos);
-      localStorage.setItem('todos', temp);
-    }, [todos]);
-   
     return (
       <TodosProvider>
         <InputTodo />
--- react/react-todo-app/src/context/TodosContext.jsx
+++ react/react-todo-app/src/context/TodosContext.jsx
@@ -10,6 +10,59 @@ import { v4 as uuidv4 } from 'uuid';
 const TodosContext = createContext(null);
 
 export const TodosProvider = ({ children }) => {
+    const [todos, setTodos] = useState(getInitialTodos());
+    const handleChange = (id) => {
+      console.log('clicked', id);
+      setTodos((prevState) => 
+          prevState.map((todo) => {
+              if (todo.id === id) {
+                  return {
+                      ...todo,
+                      completed: !todo.completed,
+                  };
+              }
+              return todo;
+          })
+      );
+  };
+  const delTodo =(id) => {
+    console.log('deleted', id);
+    setTodos([
+      ...todos.filter((todo) => {
+        return todo.id !== id;
+      }),
+    ]);
+  };
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  }; 
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle;
+        }
+        return todo;
+      })
+    );
+  };
+
+  function getInitialTodos() {
+    // getting stored items
+    const temp = localStorage.getItem('todos');
+    const savedTodos = JSON.parse(temp);
+    return savedTodos || [];
+  }
+  useEffect(() => {
+    // storing todos items
+    const temp = JSON.stringify(todos);
+    localStorage.setItem('todos', temp);
+  }, [todos]);
     return (
         <TodosContext.Provider 
             value={{
