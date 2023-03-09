5cb775649d62b0a0cf9c245112bff6e0a37f0b25
boves <daiv.boveri@gmail.com>
2023-03-08 21:41:28 -0600

    Task: Moving Todos global data to the store
    Will be broken until the next commit (consuming global data from
    the store)

--- react/react-state-mgnt-starter/src/store.js
+++ react/react-state-mgnt-starter/src/store.js
@@ -1,5 +1,39 @@
 import { create } from 'zustand';
-const todoStore = (setImmediate) => ({
-    // state data and actions will go here  
+import { v4 as uuidv4 } from 'uuid';
+const todoStore = (set) => ({
+    todos: [],
+    addTodoItem: (title) => {
+        const newTodo = {
+            id: uuidv4(),
+            title: title,
+            completed: false,
+        };
+        set((state) => ({
+            todos: [...state.todos, newTodo],
+        }));
+    },
+    delTodo: (id) => {
+        set((state) => ({
+            todos: state.todos.map((todo) => {
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed,
+                    };
+                }
+                return todo;
+            }),
+        }));
+    },
+    setUpdate: (updatedTitle, id) => {
+        set((state) => ({
+            todos: state.todos.map((todo) => {
+                if (todo.id === id) {
+                    todo.title = updatedTitle;
+                }
+                return todo;
+            }),
+        }));
+    },
 });
 export const useTodosStore = create(todoStore);
