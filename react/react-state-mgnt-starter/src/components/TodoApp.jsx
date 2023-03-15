20e6d63ffd79888ee099787d2c5c1f7bbf778e7d
boves <daiv.boveri@gmail.com>
2023-03-15 13:17:05 -0500

    Task: adding some routes for browser router

--- react/react-state-mgnt-starter/src/components/TodoApp.jsx
+++ react/react-state-mgnt-starter/src/components/TodoApp.jsx
@@ -2,13 +2,6 @@ import Header from '@/components/Header';
 import TodosLogic from '@/components/TodosLogic';
 
 const TodoApp = () => {
-  return (
-    <div className="wrapper">
-      <div className="todos">
-        <Header />
-        <TodosLogic />
-      </div>
-    </div>
-  );
+    return <></>;
 };
 export default TodoApp;
