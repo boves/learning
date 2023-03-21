7e287edfa550b9138ab0f1aae85d4041398a84e7
boves <daiv.boveri@gmail.com>
2023-03-21 12:36:41 -0500

    Task: adding Outlet component

--- react/react-state-mgnt-starter/src/components/Layout.jsx
+++ react/react-state-mgnt-starter/src/components/Layout.jsx
@@ -1,8 +1,10 @@
 import Navbar from "@/components/Navbar";
+import { Outlet } from "react-router-dom";
 const Layout = () => {
     return (
         <div className="wrapper">
             <Navbar />
+            <Outlet />
         </div>
     );
 };
