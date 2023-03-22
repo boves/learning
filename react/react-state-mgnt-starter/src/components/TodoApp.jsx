f9df1569ec57cb78e6235640c2f6fc08488f68e0
boves <daiv.boveri@gmail.com>
2023-03-22 15:15:36 -0500

    Task: adding navigation links
    For some reason, the Navbar won't show up. I tried a few fixes,
    but so far no progress. I'll have to fix this later.

--- react/react-state-mgnt-starter/src/components/TodoApp.jsx
+++ react/react-state-mgnt-starter/src/components/TodoApp.jsx
@@ -5,12 +5,12 @@ import About from '@/routes/About';
 import Login from '@/routes/Login';
 import Profile from '@/routes/Profile';
 import NotMatch from '@/routes/NotMatch';
-import Layout from '@/Layout';
+import Layout from '@/components/Layout';
 
 const TodoApp = () => {
     return (
         <Routes path ="/" element={<Layout />}>
-            <Route path="/" element={<Home />} />
+            <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="login" element={<Login />} />
             <Route path="profile" element={<Profile />} />
