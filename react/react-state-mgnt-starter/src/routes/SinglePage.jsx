6c6a312f3a1b7c7065f35a6cb20b5c82be446af0
boves <daiv.boveri@gmail.com>
2023-03-28 11:59:59 -0500

    Task: displaying dynamic content

--- react/react-state-mgnt-starter/src/routes/SinglePage.jsx
+++ react/react-state-mgnt-starter/src/routes/SinglePage.jsx
@@ -1,7 +1,22 @@
 import { useParams } from "react-router-dom";
+const aboutData = [
+    {
+      slug: 'about-app',
+      title: 'About the app',
+      description:
+        "This application lets us add to-dos, edit, and delete items. Log in to see the delete feature. It also persists to-dos in the browser's local storage for a subsequent visit.",
+    },
+    {
+      slug: 'about-developer',
+      title: 'About the developer',
+      description:
+        'Ibas Majid founded ibaslogic.com to experiment with new web features and write actionable guides. Follow Ibas on Twitter @ibaslogic to learn modern web development.',
+    },
+  ];
+  
 const SinglePage = () => {
     return (
-        console.log(useParams());
+        // console.log(useParams());
         <div className="main_content">
             single page content
         </div>
