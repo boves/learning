e71e5fa2245c2956b84bc6343ec483ad3df7e3de
boves <daiv.boveri@gmail.com>
2023-03-27 23:04:59 -0500

    Task: reading URL params with useParams hook

--- react/react-state-mgnt-starter/src/routes/SinglePage.jsx
+++ react/react-state-mgnt-starter/src/routes/SinglePage.jsx
@@ -1,5 +1,7 @@
+import { useParams } from "react-router-dom";
 const SinglePage = () => {
     return (
+        console.log(useParams());
         <div className="main_content">
             single page content
         </div>
