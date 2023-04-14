ba22671a20a718ddf25d9417de9afb0ed1870dbe
boves <daiv.boveri@gmail.com>
2023-04-13 21:05:17 -0500

    Task: persist user state in browser storage

--- react/react-state-mgnt-starter/src/context/AuthContext.jsx
+++ react/react-state-mgnt-starter/src/context/AuthContext.jsx
@@ -6,6 +6,17 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState('');
     const login = (user) => setUser(user);
     const logout = () => setUser(null);
+    function getUsername() {
+        // getting stored state
+        const temp = localStorage.getItem('username');
+        const savedUsername = JSON.parse(temp);
+        return savedUsername || '';
+    }
+    useEffect(() => {
+        // storing user state
+        const temp = JSON.stringify(user);
+        localStorage.setItem('username', temp);
+    }, [user]);
     return(
         <AuthContext.Provider value={{ user, login, logout }}>
             {children}
