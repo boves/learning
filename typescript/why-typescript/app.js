e3eb815625f737c6e95f4695e8b8a722c389d294
boves <daiv.boveri@gmail.com>
2023-06-29 13:19:37 -0500

    Task: understanding dynamic types in JS

--- typescript/why-typescript/app.js
+++ typescript/why-typescript/app.js
@@ -6,3 +6,14 @@ console.log(typeof(box)); // string
 
 box = 100;
 console.log(typeof(box)); // number
+
+num = 2020;
+
+let box2;
+console.log(typeof(box)); // undefined
+
+box2 = "Hello";
+console.log(typeof(box2)); // string
+
+box2 = 100;
+console.log(typeof(box2)); // number
