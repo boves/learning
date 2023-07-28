3c34e460ddb8d5e836ef4003cc14b7f59248dbdd
boves <daiv.boveri@gmail.com>
2023-07-28 18:19:00 -0500

    Task: Setting up CSS styles and updating App.tsx

--- typescript/keeptrack/src/App.tsx
+++ typescript/keeptrack/src/App.tsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React, and make your dreams come true!
-        </a>
-        <p>
-          Or, I mean, like, steal some money.
-        </p>
-      </header>
-    </div>
-  );
+    <blockquote cite="Benjamin Franklin">
+      Tell me and I forgot, teach me and I may remember, involve me and I learn.
+    </blockquote>
+  )
 }
 
 export default App;
