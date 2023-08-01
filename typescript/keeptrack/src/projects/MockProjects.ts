473193297d5264a052e4cfa40e682ca080256f2f
boves <daiv.boveri@gmail.com>
2023-08-01 09:59:43 -0500

    Minor update: formatting

--- typescript/keeptrack/src/projects/MockProjects.ts
+++ typescript/keeptrack/src/projects/MockProjects.ts
@@ -3,14 +3,14 @@ import { Project } from './Project';
 export const MOCK_PROJECTS = [
     new Project ({
         id: 1,
-        name: 'Johnson – Kutch',
+        name: 'Johnson - Kutch',
         description: 
             'Fully-configurable intermediate framework. Ullam occaecati libero laudantium nihil voluptas omnis.',
         imageUrl: '/assets/placeimg_500_300_arch4.jpg',
         contractTypeId: 3,
         contractSignedOn: '2013-08-04T22:39:41.473Z',
         budget: 54637,
-        isActive: false;
+        isActive: false,
     }),
     new Project ({
         id: 2,
